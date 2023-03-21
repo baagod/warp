b34ab1dbb9b0d66d7d27002125f0e7a964bf4c96
78081 <780810441@qq.com>
2023-03-21 23:20:00 +0800

    删除 timer.go

--- beam_test.go
+++ beam_test.go
@@ -1,15 +1,7 @@
 package beam_test
 
 import (
-	"fmt"
-	"github.com/baa-god/beam"
 	"testing"
 )
 
-func TestFunc(_ *testing.T) {
-	now := beam.Now()
-
-	fmt.Println(now.Before(beam.Parse("2024"))) // 在指定时间之前 (t < u): true
-	fmt.Println(now.After(beam.Parse("2024")))  // 在指定时间之后 (t > u): false
-	fmt.Println(now.Equal(beam.Parse("2024")))  // 是否相等: false
-}
+func TestFunc(t *testing.T) {}
